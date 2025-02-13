9aecf9552a8de7b53849079cca3560380a464e29
KANE <netmorphus@hotmail.com>
2025-02-13 18:11:12 +0100

    Mise à jour zoneController et zoneRoutes

--- routes/zoneRoutes.js
+++ routes/zoneRoutes.js
@@ -1,31 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const zoneController = require('../controllers/zoneController');
-// const authMiddleware = require('../middleware/authMiddleware');
-// const roleMiddleware = require('../middleware/roleMiddleware');
-
-// // Ajouter une zone
-// router.post('/', authMiddleware, roleMiddleware('admin'), zoneController.addZone);
-
-// // Récupérer toutes les zones
-// router.get('/', authMiddleware, zoneController.getAllZones);
-
-// // Modifier une zone
-// router.put('/:id', authMiddleware, roleMiddleware('admin'), zoneController.updateZone);
-
-// // Supprimer une zone
-// router.delete('/:id', authMiddleware, roleMiddleware('admin'), zoneController.deleteZone);
-
-
-// router.get('/zones', authMiddleware, roleMiddleware('collector'), zoneController.getZones);
-
-// module.exports = router;
-
-
-
-
-
-
 const express = require('express');
 const router = express.Router();
 const { createZone, getAllZones } = require('../controllers/zoneController');
