41eaae6b2bdaed5083ad71cdf75d77754f22097a
KANE <netmorphus@hotmail.com>
2025-02-15 14:09:32 +0100

    Correction de CORS3

--- server.js
+++ server.js
@@ -32,6 +32,21 @@ app.use(cors({
   credentials: true
 }));
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Réponse immédiate pour éviter l'erreur 204
+  }
+
+  next();
+});
+
+
 app.use(express.json()); // Parser les données JSON
 
 // Logger toutes les requêtes
